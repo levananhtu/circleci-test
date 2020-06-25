dd0f9ef3c2e9fd71e69171244c64261645f75caa
Tu LVA <27-tulva@users.noreply.gitlab.datasection.com.vn>
2020-06-25 14:11:30 +0700

    update config.yml

---
+++ src/test/java/protest/circlecitest/CircleciTestApplicationTests.java
@@ -0,0 +1,13 @@
+package protest.circlecitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleciTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
