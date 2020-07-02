f8fa159a42a8067142b5d775794f881a81cd238f
Tu LVA <27-tulva@users.noreply.gitlab.datasection.com.vn>
2020-07-02 13:13:30 +0700

    update

--- src/main/java/protest/circlecitest/CircleciTestApplication.java
+++ src/main/java/protest/circlecitest/CircleciTestApplication.java
@@ -2,12 +2,27 @@ package protest.circlecitest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
+@RestController
 public class CircleciTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CircleciTestApplication.class, args);
     }
 
+    @GetMapping(path = "/AB")
+    public String A(HttpServletRequest request, HttpServletResponse response) {
+        return request.getRequestURI() + "1";
+    }
+
+    @GetMapping(path = "/ab")
+    public String a(HttpServletRequest request, HttpServletResponse response) {
+        return request.getRequestURI() + "2";
+    }
 }
--- src/test/java/protest/circlecitest/CircleciTestApplicationTests.java
+++ src/test/java/protest/circlecitest/CircleciTestApplicationTests.java
@@ -1,13 +1,27 @@
 package protest.circlecitest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class CircleciTestApplicationTests {
 
     @Test
-    void contextLoads() {
+    void first() throws IOException {
+        Assertions.assertEquals(2, 2);
+    }
+
+    @Test
+    void second() throws IOException {
+        Assertions.assertEquals(2, 2);
+    }
+
+    @Test
+    void third() throws IOException {
+        Assertions.assertEquals(2, 2);
     }
 
 }
