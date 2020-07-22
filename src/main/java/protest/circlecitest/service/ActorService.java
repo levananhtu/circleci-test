377e2cea49e9e2bc72e5df78564d401c524ce610
Tu LVA <27-tulva@users.noreply.gitlab.datasection.com.vn>
2020-07-22 15:10:17 +0700

    update

--- src/main/java/protest/circlecitest/service/ActorService.java
+++ src/main/java/protest/circlecitest/service/ActorService.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Service;
 import protest.circlecitest.entity.Actor;
 import protest.circlecitest.repository.ActorRepository;
 
+import java.util.logging.Logger;
+
 @Service
 public class ActorService {
+    private static final Logger LOGGER = Logger.getLogger(ActorService.class.getName());
     private final ActorRepository actorRepository;
 
     public ActorService(ActorRepository actorRepository) {
